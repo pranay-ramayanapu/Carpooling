f33a7ee05cbaacea27bf685eab2af61d72e40a2f
RAMAYANAPU VENKATA PRANAI <pranayramayanapu@gmail.com>
2026-05-18 13:30:36 +0530

    Revert "redis config check"
    This reverts commit c0e97258442c42a81b10cd7e978bf6543b41c3b3.

--- src/main/java/com/example/carpooling/config/RedisConfig.java
+++ src/main/java/com/example/carpooling/config/RedisConfig.java
@@ -12,7 +12,6 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
-import java.net.URI;
 
 @Configuration
 public class RedisConfig {
@@ -26,52 +25,16 @@ public class RedisConfig {
     @Value("${REDIS_PORT}")
     private int redisPort;
 
-    @Value("${spring.redis.ssl:false}")
-    private boolean redisSsl;
-
-    @Value("${REDIS_URI:}")
-    private String redisUri;
-
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        JedisClientConfiguration jedisClientConfiguration = redisSsl
-                ? JedisClientConfiguration.builder()
-                        .connectTimeout(Duration.ofSeconds(2))
-                        .readTimeout(Duration.ofSeconds(2))
-                        .useSsl()
-                        .build()
-                : JedisClientConfiguration.builder()
-                        .connectTimeout(Duration.ofSeconds(2))
-                        .readTimeout(Duration.ofSeconds(2))
-                        .build();
-
-        RedisStandaloneConfiguration redisStandaloneConfiguration = createRedisConfiguration();
+        JedisClientConfiguration jedisClientConfiguration = JedisClientConfiguration.builder()
+                .connectTimeout(Duration.ofSeconds(2))
+                .readTimeout(Duration.ofSeconds(2))
+                .build();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost,
+                redisPort);
+        redisStandaloneConfiguration.setPassword(redisPassword);
         return new JedisConnectionFactory(redisStandaloneConfiguration, jedisClientConfiguration);
-    }
-
-    private RedisStandaloneConfiguration createRedisConfiguration() {
-        if (redisUri != null && !redisUri.isBlank()) {
-            URI uri = URI.create(redisUri);
-            RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-            configuration.setHostName(uri.getHost());
-            configuration.setPort(uri.getPort() > 0 ? uri.getPort() : redisPort);
-
-            String userInfo = uri.getUserInfo();
-            if (userInfo != null && userInfo.contains(":")) {
-                configuration.setPassword(org.springframework.data.redis.connection.RedisPassword
-                        .of(userInfo.substring(userInfo.indexOf(':') + 1)));
-            } else if (redisPassword != null && !redisPassword.isBlank()) {
-                configuration.setPassword(org.springframework.data.redis.connection.RedisPassword.of(redisPassword));
-            }
-
-            return configuration;
-        }
-
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost, redisPort);
-        if (redisPassword != null && !redisPassword.isBlank()) {
-            configuration.setPassword(org.springframework.data.redis.connection.RedisPassword.of(redisPassword));
-        }
-        return configuration;
     }
 
     @Bean
